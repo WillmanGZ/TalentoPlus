a399c8a99baffafdb0eb0b47a89401b01c2bd701
WillmanGZ <willmang@uninorte.edu.co>
2025-12-09 13:33:32 -0500

    feat: add base razor project

---
+++ RRHH.WEB/Pages/Index.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RRHH.WEB.Pages;
+
+public class IndexModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
---
+++ RRHH.WEB/Pages/Privacy.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RRHH.WEB.Pages;
+
+public class PrivacyModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
