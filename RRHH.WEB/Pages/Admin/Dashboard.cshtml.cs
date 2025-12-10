d467424c19e6180f28fed5af5f026f57d72c89f7
WillmanGZ <willmang@uninorte.edu.co>
2025-12-09 19:11:08 -0500

    feat: dashboard

--- RRHH.WEB/Pages/Admin/Dashboard.cshtml.cs
+++ RRHH.WEB/Pages/Admin/Dashboard.cshtml.cs
@@ -1,45 +1,41 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.AspNetCore.Authorization;
+using Microsoft.EntityFrameworkCore;
+using RRHH.WEB.Data;
+using RRHH.WEB.Data.Entities;
 
 namespace RRHH.WEB.Pages.Admin
 {
     public class DashboardModel : PageModel
     {
-        // private readonly AppDbContext _context;
-        // private readonly UserManager<IdentityUser> _userManager;
-        //
-        // public DashboardModel(AppDbContext context, UserManager<IdentityUser> userManager)
-        // {
-        //     _context = context;
-        //     _userManager = userManager;
-        // }
-        //
-        // public int TotalProducts { get; set; }
-        // public int TotalSales { get; set; }
-        // public int TotalClients { get; set; }
-        // public decimal TotalRevenue { get; set; }
-        //
-        // public async Task OnGetAsync()
-        // {
-        //     TotalProducts = await _context.Products.CountAsync();
-        //     TotalSales = await _context.Sales.CountAsync();
-        //     TotalRevenue = await _context.SaleProducts.SumAsync(sp => sp.UnitPrice * sp.Quantity);
-        //
-        //     // Contar solo usuarios con rol "Client"
-        //     var allUsers = _userManager.Users.ToList();
-        //     int clientCount = 0;
-        //
-        //     foreach (var user in allUsers)
-        //     {
-        //         var roles = await _userManager.GetRolesAsync(user);
-        //         if (roles.Contains("Client"))
-        //             clientCount++;
-        //     }
-        //
-        //     TotalClients = clientCount;
-        // }
+        private readonly AppDbContext _context;
+        private readonly UserManager<Employee> _userManager;
+
+        public int TotalEmployees { get; set; }
+
+        public DashboardModel(AppDbContext context, UserManager<Employee> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        public async Task OnGetAsync()
+        {
+            var allUsers = await _userManager.Users.ToListAsync();
+            int employeeCount = 0;
+
+            foreach (var user in allUsers)
+            {
+
+                var roles = await _userManager.GetRolesAsync(user);
+
+                if (roles.Contains("Employee"))
+                {
+                    employeeCount++;
+                }
+            }
+
+            TotalEmployees = employeeCount;
+        }
     }
 }
-
-
-
